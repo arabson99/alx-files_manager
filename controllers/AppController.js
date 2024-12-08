cba9260cc838e26319707871ffe83820ad000189
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-12-08 08:51:56 +0100

    Task 2. First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,25 +3,17 @@ import redisClient from '../utils/redis';
 
 class AppController {
   static getStatus(request, response) {
-    try {
       const redis = redisClient.isAlive();
       const db = dbClient.isAlive();
 
       response.status(200).json({ redis, db });
-    } catch (error) {
-      response.status(500).json({ error });
-    }
   }
 
   static async getStats(request, response) {
-    try {
       const users = await dbClient.nbUsers();
       const files = await dbClient.nbFiles();
 
       response.status(200).json({ users, files });
-    } catch (error) {
-      response.status(500).json({ error });
-    }
   }
 }
 
